284e82edde938b25461af6b99b6fb6c3440ac537
Chaurasiya <uchaurasiya@deloitte.com>
2020-09-18 11:41:53 +0530

    Ticket Booking

--- src/main/java/com/deloitte/ticketbookingservice/TicketbookingServiceApplication.java
+++ src/main/java/com/deloitte/ticketbookingservice/TicketbookingServiceApplication.java
@@ -30,21 +30,37 @@ public class TicketbookingServiceApplication {
 	
 	 @Bean
 	    ApplicationRunner init(IBusRepository repository, IBusBookingRepository busBookingRepository) throws ParseException {
-		 //repository.deleteAll();
-		 //busBookingRepository.deleteAll();
+		 repository.deleteAll();
+		 busBookingRepository.deleteAll();
 		 List<Bus> busList = new ArrayList<Bus>();
-		 busList.add(new Bus("AC", "Hyderabad", 0,"V Kaveri Travels", new Route("Delhi", "Mumbai", "04:00", "20:30", "2020-09-18"),1700));
-		 busList.add(new Bus("AC", "Bangalore", 0, "SRS Travels", new Route("Bangalore", "Goa", "05:30", "15:00", "2020-09-20"),2000));
-		 busList.add(new Bus("NonAC", "Goa", 0, "VRL Travels", new Route("Hydrabad", "Mumbai", "11:45", "21:00", "2020-10-15"),1800));
-		 busList.add(new Bus("AC", "Hyderabad", 0,"V Kaveri Travels", new Route("Delhi", "Mumbai", "13:00", "23:30", "2020-09-21"),1700));
-		 busList.add(new Bus("AC", "Bangalore", 0, "SRS Travels", new Route("Bangalore", "Goa", "16:30", "22:00", "2020-09-17"),2000));
-		 busList.add(new Bus("NonAC", "Goa", 0, "VRL Travels", new Route("Hydrabad", "Mumbai", "07:45", "21:00", "2020-10-20"),1800));
-		 busList.add(new Bus("AC", "Hyderabad", 0,"V Kaveri Travels", new Route("Delhi", "Mumbai", "04:00", "20:30", "2020-10-18"),1700));
-		 busList.add(new Bus("AC", "Bangalore", 0, "SRS Travels", new Route("Bangalore", "Goa", "05:30", "15:00", "2020-10-20"),2000));
-		 busList.add(new Bus("NonAC", "Goa", 0, "VRL Travels", new Route("Hydrabad", "Mumbai", "11:45", "21:00", "2020-10-05"),1800));
+		 busList.add(new Bus("AC", "Delhi", 0,"V Kaveri Travels", 
+				 new Route("Delhi", "Mumbai", "04:00", "20:30", "2020-09-21"),1700));
+		 busList.add(new Bus("AC", "Bangalore", 0, "SRS Travels", 
+				 new Route("Bangalore", "Goa", "05:30", "15:00", "2020-09-21"),2000));
+		 busList.add(new Bus("Non-AC", "Hydrabad", 0, "VRL Travels", 
+				 new Route("Hydrabad", "Mumbai", "11:45", "21:00", "2020-09-21"),1800));
+		 busList.add(new Bus("AC", "Mumbai", 0,"V Kaveri Travels", 
+				 new Route("Delhi", "Mumbai", "13:00", "23:30", "2020-09-22"),1700));
+		 busList.add(new Bus("AC", "Bangalore", 0, "SRS Travels", 
+				 new Route("Bangalore", "Goa", "16:30", "22:00", "2020-09-22"),2000));
+		 busList.add(new Bus("Non-AC", "Mumbai", 0, "VRL Travels", 
+				 new Route("Hydrabad", "Mumbai", "07:45", "21:00", "2020-09-22"),1800));
+		 busList.add(new Bus("AC", "Delhi", 0,"V Kaveri Travels", 
+				 new Route("Delhi", "Mumbai", "04:00", "20:30", "2020-09-23"),1700));
+		 busList.add(new Bus("AC", "Bangalore", 0, "SRS Travels", 
+				 new Route("Bangalore", "Goa", "05:30", "15:00", "2020-09-23"),2000));
+		 busList.add(new Bus("Non-AC", "Hydrabad", 0, "VRL Travels", 
+				 new Route("Hydrabad", "Mumbai", "11:45", "21:00", "2020-09-23"),1800));
+		 busList.add(new Bus("Sleeper", "Delhi", 0,"V Kaveri Travels", 
+				 new Route("Delhi", "Mumbai", "04:00", "20:30", "2020-09-21"),1200));
+		 busList.add(new Bus("Sleeper", "Bangalore", 0, "V Kaveri Travels", 
+				 new Route("Bangalore", "Goa", "05:30", "15:00", "2020-09-21"),1500));
+		 busList.add(new Bus("Sleeper", "Hydrabad", 0, "V Kaveri Travels", 
+				 new Route("Hydrabad", "Mumbai", "11:45", "21:00", "2020-09-21"),1200));
+
 		 return args -> {
 	            busList.stream().forEach(bus -> {
-	                //repository.save(bus);
+	                repository.save(bus);
 	            });
 	            repository.findAll().forEach(System.out::println);
 	        };
